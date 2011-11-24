59b685698f987d8f3792fbbf0ed160bc92e4e6c0
servkey <servkey@hotmail.com>
2011-11-24 12:03:13 -0600

    Merge DB

---
+++ ChangeIPAddressLibrary/Core/DBLiteConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+
+namespace ChangeIPAddressLibrary.Core
+{
+    public class DBLiteConnection
+    {
+        private string strConnection;
+
+        public DBLiteConnection()
+        {
+            strConnection = @"Data Source=.\Setting\settings.s3db";
+        }
+
+        public int ExecuteNonQuery(string sql)
+        {
+            int rows = 0;
+            SQLiteConnection con = new SQLiteConnection(strConnection);
+            con.Open();
+            SQLiteCommand command = new SQLiteCommand();
+            command.CommandText = sql;
+            rows = command.ExecuteNonQuery();
+            con.Close();
+            return rows;
+        }
+
+    
+    }
+}
--- README.txt
+++ README.txt
@@ -1,1 +1,2 @@
+
 Proyect ChangeIP
